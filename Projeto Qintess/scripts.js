d698e01669541a6d7ed0dc28e3cdd38336ccdf23
Douglasjin <30833139+douglasjim@users.noreply.github.com>
2023-04-27 18:03:19 -0300

    Add files via upload

---
+++ Projeto Qintess/scripts.js
@@ -0,0 +1,12 @@
+function telaLogin(){
+    window.location.href = "login.html";
+}
+
+function telaCadastramento(){
+    window.location.href = "cadastramento.html";
+}
+
+function voltarHome(){
+    window.location.href = "paginaPrincipal.html";
+}
+
